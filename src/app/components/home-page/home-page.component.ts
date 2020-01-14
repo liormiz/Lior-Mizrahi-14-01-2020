e63142e9d03ab2f5b2c0a048c7c82d878a2d856e
lior mizrahi <liorstrongxd@gmail.com>
2020-01-14 21:20:40 +0200

    auto complete working !!

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LOCATIONS_OUT_COMPLETE } from 'src/app/config/consts';
+import {  WeatherService} from '../../services/weather/weather.service';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home-page',
@@ -7,10 +10,32 @@ import { LOCATIONS_OUT_COMPLETE } from 'src/app/config/consts';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  myControl : FormControl;
+  options : string[];
 
-  constructor() { }
+  constructor(private WeatherService : WeatherService) { }
 
   ngOnInit() {
-    
+    this.myControl = new FormControl();
+    this.options;
+  }
+
+  getCitiesAutoComplete(event : any){
+    let citiesToSearch = event.currentTarget.value;
+    this.WeatherService.getCitiesByCity(citiesToSearch).subscribe(data =>{
+      this.options = this.getCityNames(data);
+    });
+  }
+
+  getCityNames(data : any) : string[]{
+    let cities : any = [];
+
+    if (data){
+    for (let nIndex = 0; nIndex < data.length; nIndex++){
+      cities.push(data[nIndex].LocalizedName);
+      }
+    }
+    return cities;
+
   }
 }
--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { API_KEY, LOCATIONS_OUT_COMPLETE } from 'src/app/config/consts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  todayDate() {
+    let ndate = new Date();
+    return ndate;
+  }
+
+  getCitiesByCity(cityName : string) {
+    let apiKey = API_KEY;
+    let url = LOCATIONS_OUT_COMPLETE;
+    let data = this.http.get(url + "?apikey=" + apiKey + "&q=" + cityName);
+    return data;
+  }
 }
